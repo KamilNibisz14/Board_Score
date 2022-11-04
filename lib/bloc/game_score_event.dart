3b40dfa5085f4f3d85758d61a7a7ed39a3263d6c
mateszw <mateuszs8892@gmail.com>
2022-11-04 14:41:03 +0100

    Small bloc refactor & persis game score storage

--- lib/bloc/game_score_event.dart
+++ lib/bloc/game_score_event.dart
@@ -7,40 +7,40 @@ abstract class GameScoreEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class LoadGameEvent extends GameScoreEvent{}
+class LoadGameEvent extends GameScoreEvent {}
 
-class SearchGameIndex extends GameScoreEvent{
+class SearchGameIndex extends GameScoreEvent {
   final String gameID;
   const SearchGameIndex({required this.gameID});
-  
+
   @override
   List<Object> get props => [gameID];
 }
 
-class AddPlayerData extends GameScoreEvent{
+class AddPlayerData extends GameScoreEvent {
   final PlayerData playerData;
   const AddPlayerData({required this.playerData});
 
   @override
   List<Object> get props => [playerData];
 }
-class AddPoints extends GameScoreEvent{
+
+class AddPoints extends GameScoreEvent {
   final PlayerData playerData;
   const AddPoints({required this.playerData});
 
   @override
   List<Object> get props => [playerData];
 }
-class RemovePoint extends GameScoreEvent{
+
+class RemovePoint extends GameScoreEvent {
   final PlayerData playerData;
   const RemovePoint({required this.playerData});
   @override
   List<Object> get props => [playerData];
 }
 
-
-
-class DeletePlayer extends GameScoreEvent{
+class DeletePlayer extends GameScoreEvent {
   final PlayerData playerData;
   const DeletePlayer({required this.playerData});
 
--- lib/widgets/player_list_view.dart
+++ lib/widgets/player_list_view.dart
@@ -4,54 +4,45 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/game_score_bloc.dart';
 
-class PlayerListView extends StatefulWidget {
-  const PlayerListView({Key? key}) : super(key: key);
+class PlayerListView extends StatelessWidget {
+  const PlayerListView({super.key});
 
-  @override
-  State<PlayerListView> createState() => _PlayerListViewState();
-}
-
-class _PlayerListViewState extends State<PlayerListView> {
   @override
   Widget build(BuildContext context) {
-    refresh() {
-      setState(() {});
-    }
     double screenWidth = MediaQuery.of(context).size.width;
     double leadingPlayerNameFontSize = screenWidth / 22;
     return BlocBuilder<GameScoreBloc, GameScoreState>(
         builder: (context, state) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 15),
-                child: Text(
-                  'leading:  ${state.gameData!.winner}',
-                  textAlign: TextAlign.left,
-                  style:
-                  TextStyle(fontSize: leadingPlayerNameFontSize),
-                ),
-              ),
-              ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: state.gameData!.players.length,
-                  itemBuilder: (context, index){
-                    return InkWell(
-                      onLongPress: (){
-                        setState(() {
-                          context.read<GameScoreBloc>().add(DeletePlayer(playerData: state.gameData!.players[index]));
-                        });
-                      },
-                      child: CurrentPlayerData(
-                        playerData: state.gameData!.players[index],notifyParent: refresh
-                      ),
-                    );
-                  }
-              ),
-            ],
-          );
-        });
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              'leading:  ${state.gameData[state.currentIndex].winner}',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: leadingPlayerNameFontSize),
+            ),
+          ),
+          ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: state.gameData[state.currentIndex].players.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onLongPress: () {
+                    context.read<GameScoreBloc>().add(DeletePlayer(
+                        playerData:
+                            state.gameData[state.currentIndex].players[index]));
+                  },
+                  child: CurrentPlayerData(
+                    playerData:
+                        state.gameData[state.currentIndex].players[index],
+                  ),
+                );
+              }),
+        ],
+      );
+    });
   }
 }
